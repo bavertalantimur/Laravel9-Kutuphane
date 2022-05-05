4044de1d4cc8e902f81060738af121c9acf60925
baver <bawer.talantimur@gmail.com>
2022-05-05 05:37:40 +0300

    admin login işlemleri

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -10,6 +11,32 @@ class HomeController extends Controller
     {
         return view('home.index');
     }
+    public function login()
+    {
+        return view('admin.login');
+    }
+
+    public function logincheck(Request $request)
+    {
+        if ($request->isMethod('post'))
+        {
+            $credentials = $request->only('email','password');
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+
+                return redirect()->intended('admin');
+            }
+
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ]);
+        }
+        else{
+            return view('admin.login');
+        }
+    }
+
 
     public function aboutus()
     {
